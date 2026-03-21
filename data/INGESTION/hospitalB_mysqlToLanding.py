f938897fd1e377dce720486fa6f2994a2b715c1c
sai charan <saicharanrao.work19@gmail.com>
2026-03-21 13:47:34 -0400

    changes in the db file

--- data/INGESTION/hospitalB_mysqlToLanding.py
+++ data/INGESTION/hospitalB_mysqlToLanding.py
@@ -26,10 +26,10 @@ BQ_TEMP_PATH = f"{GCS_BUCKET}/temp/"
 
 # MySQL Configuration
 MYSQL_CONFIG = {
-    "url": "jdbc:mysql://34.59.188.6:3306/hospital_b_db?useSSL=false&allowPublicKeyRetrieval=true",
+    "url": "jdbc:mysql://136.115.82.148:3306/hosptial-b-db?useSSL=false&allowPublicKeyRetrieval=true",
     "driver": "com.mysql.cj.jdbc.Driver",
     "user": "myuser",
-    "password": "mypass"
+    "password": "P@ssword0@"
 }
 
 ##------------------------------------------------------------------------------------------------------------------##
